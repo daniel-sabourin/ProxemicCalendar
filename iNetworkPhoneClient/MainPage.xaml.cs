7e72baff94c31fe6747e595aa37061b454eacc68
Daniel Sabourin <sabourid@ucalgary.ca>
2013-11-25 17:13:24 -0700

    Added basic layout, as well as starting photo chooser

--- iNetworkPhoneClient/MainPage.xaml.cs
+++ iNetworkPhoneClient/MainPage.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
+using Microsoft.Phone.Tasks;
+
 using GroupLab.iNetwork;
 using GroupLab.iNetwork.Tcp;
 
@@ -22,6 +24,8 @@ namespace iNetworkPhoneClient
         private Connection _connection;
         private string _ipAddress = "127.0.0.1";
         private int _port = 12345;
+
+        PhotoChooserTask photoChooserTask;
 
         #region iNetwork Methods
 
@@ -71,10 +75,32 @@ namespace iNetworkPhoneClient
             InitializeComponent();
             InitializeConnection();
 
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
+
             // to send a message 
             // Message msg = new Message("Name-of-Message");
             // msg.AddField("Name-of-Field", 0);
             // this._connection.SendMessage(msg);
         }
+
+        private void Image_Tap(object sender, GestureEventArgs e)
+        {
+            photoChooserTask.Show();
+        }
+
+        void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                MessageBox.Show(e.ChosenPhoto.Length.ToString());
+
+                //Code to display the photo on the page in an image control named myImage.
+                //System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
+                //bmp.SetSource(e.ChosenPhoto);
+                //myImage.Source = bmp;
+            }
+        }
+
     }
 }
